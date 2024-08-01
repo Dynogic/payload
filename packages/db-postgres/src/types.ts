Dynogic/payload

commit 075819964d6598395759abd326872d6849323576

fix(db-postgres, db-sqlite): enum schema (#7453)
- updates drizzle-kit and drizzle-orm
- fix enum creation to fully support custom schemas
- sqlite by default will not use transactions

packages/db-postgres/src/types.ts
@@ -21,6 +21,7 @@ import type {
   PgSchema,
   PgTableWithColumns,
   PgTransactionConfig,
+  pgEnum,
 } from 'drizzle-orm/pg-core'
 import type { PgTableFn } from 'drizzle-orm/pg-core/table'
 import type { Payload, PayloadRequest } from 'payload'
@@ -106,6 +107,13 @@ type PostgresDrizzleAdapter = Omit<
   | 'relations'
 >
 
+type Schema =
+  | {
+      enum: typeof pgEnum
+      table: PgTableFn
+    }
+  | PgSchema
+
 export type PostgresAdapter = {
   countDistinct: CountDistinct
   defaultDrizzleSnapshot: DrizzleSnapshotJSON
@@ -125,7 +133,7 @@ export type PostgresAdapter = {
   localesSuffix?: string
   logger: DrizzleConfig['logger']
   operators: Operators
-  pgSchema?: { table: PgTableFn } | PgSchema
+  pgSchema?: Schema
   pool: Pool
   poolOptions: Args['pool']
   push: boolean
@@ -133,7 +141,7 @@ export type PostgresAdapter = {
   relations: Record<string, GenericRelation>
   relationshipsSuffix?: string
   resolveInitializing: () => void
-  schema: Record<string, GenericEnum | GenericRelation | GenericTable>
+  schema: DrizzleConfig
   schemaName?: Args['schemaName']
   sessions: {
     [id: string]: {

packages/db-sqlite/src/types.ts
@@ -1,10 +1,10 @@
 import type { Client, Config, ResultSet } from '@libsql/client'
 import type { Operators } from '@payloadcms/drizzle'
 import type { BuildQueryJoinAliases, DrizzleAdapter } from '@payloadcms/drizzle/types'
-import type { ColumnDataType, DrizzleConfig, Relation, Relations, SQL } from 'drizzle-orm'
+import type { DrizzleConfig, Relation, Relations, SQL } from 'drizzle-orm'
 import type { LibSQLDatabase } from 'drizzle-orm/libsql'
 import type {
-  SQLiteColumn,
+  AnySQLiteColumn,
   SQLiteInsertOnConflictDoUpdateConfig,
   SQLiteTableWithColumns,
   SQLiteTransactionConfig,
@@ -25,24 +25,8 @@ export type Args = {
   versionsSuffix?: string
 }
 
-export type GenericColumn = SQLiteColumn<
-  {
-    baseColumn: never
-    columnType: string
-    data: unknown
-    dataType: ColumnDataType
-    driverParam: unknown
-    enumValues: string[]
-    hasDefault: false
-    name: string
-    notNull: false
-    tableName: string
-  },
-  object
->
-
 export type GenericColumns = {
-  [x: string]: GenericColumn
+  [x: string]: AnySQLiteColumn
 }
 
 export type GenericTable = SQLiteTableWithColumns<{

packages/drizzle/src/types.ts
@@ -120,6 +120,7 @@ export type RequireDrizzleKit = () => {
   pushSchema: (
     schema: Record<string, unknown>,
     drizzle: DrizzleAdapter['drizzle'],
+    filterSchema?: string[],
   ) => Promise<{ apply; hasDataLoss; warnings }>
 }
 

.github/workflows/main.yml
@@ -196,6 +196,7 @@ jobs:
           - postgres-custom-schema
           - postgres-uuid
           - supabase
+          - sqlite
     env:
       POSTGRES_USER: postgres
       POSTGRES_PASSWORD: postgres

packages/db-postgres/src/connect.ts
@@ -53,6 +53,7 @@ export const connect: Connect = async function connect(
   const { hotReload } = options
 
   this.schema = {
+    pgSchema: this.pgSchema,
     ...this.tables,
     ...this.relations,
     ...this.enums,

packages/db-postgres/src/createMigration.ts
@@ -49,6 +49,12 @@ export const createMigration: CreateMigration = async function createMigration(
 
   let drizzleJsonBefore = defaultDrizzleSnapshot
 
+  if (this.schemaName) {
+    drizzleJsonBefore.schemas = {
+      [this.schemaName]: this.schemaName,
+    }
+  }
+
   if (!upSQL) {
     // Get latest migration snapshot
     const latestSnapshot = fs

packages/db-postgres/src/defaultSnapshot.ts
@@ -7,10 +7,11 @@ export const defaultDrizzleSnapshot: DrizzleSnapshotJSON = {
     schemas: {},
     tables: {},
   },
-  dialect: 'pg',
+  dialect: 'postgresql',
   enums: {},
   prevId: '00000000-0000-0000-0000-00000000000',
   schemas: {},
+  sequences: {},
   tables: {},
-  version: '5',
+  version: '7',
 }

packages/db-postgres/src/index.ts
@@ -32,6 +32,7 @@ import {
   updateOne,
   updateVersion,
 } from '@payloadcms/drizzle'
+import { type PgSchema, pgEnum, pgSchema, pgTable } from 'drizzle-orm/pg-core'
 import { createDatabaseAdapter } from 'payload'
 
 import type { Args, PostgresAdapter } from './types.js'
@@ -62,11 +63,18 @@ export function postgresAdapter(args: Args): DatabaseAdapterObj<PostgresAdapter>
     const migrationDir = findMigrationDir(args.migrationDir)
     let resolveInitializing
     let rejectInitializing
+    let adapterSchema: PostgresAdapter['pgSchema']
 
     const initializing = new Promise<void>((res, rej) => {
       resolveInitializing = res
       rejectInitializing = rej
     })
+
+    if (args.schemaName) {
+      adapterSchema = pgSchema(args.schemaName)
+    } else {
+      adapterSchema = { enum: pgEnum, table: pgTable }
+    }
 
     return createDatabaseAdapter<PostgresAdapter>({
       name: 'postgres',
@@ -83,7 +91,7 @@ export function postgresAdapter(args: Args): DatabaseAdapterObj<PostgresAdapter>
       localesSuffix: args.localesSuffix || '_locales',
       logger: args.logger,
       operators: operatorMap,
-      pgSchema: undefined,
+      pgSchema: adapterSchema,
       pool: undefined,
       poolOptions: args.pool,
       push: args.push,

commit 143ec7570f37d8c978a304860452f4dc78f09f0a

fix(db-postgres): incorrect schema type on adapter (#7459)
fixes a db-postgres type issue that was introduced in
https://github.com/payloadcms/payload/pull/7453

packages/db-postgres/src/types.ts
@@ -141,7 +141,6 @@ export type PostgresAdapter = {
   relations: Record<string, GenericRelation>
   relationshipsSuffix?: string
   resolveInitializing: () => void
-  schema: DrizzleConfig
   schemaName?: Args['schemaName']
   sessions: {
     [id: string]: {
@@ -183,7 +182,7 @@ declare module 'payload' {
     rejectInitializing: () => void
     relationshipsSuffix?: string
     resolveInitializing: () => void
-    schema: Record<string, GenericEnum | GenericRelation | GenericTable>
+    schema: Record<string, unknown>
     schemaName?: Args['schemaName']
     tableNameMap: Map<string, string>
     versionsSuffix?: string